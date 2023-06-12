be26cfc7ea0a2ac620056ab74118ecd5eade8ed7
AdamKoziorz <adam-koziorz@outlook.com>
2023-06-12 17:49:50 -0400

    Fix display issue and other bugs
    The calculator is now centered in the middle of the screen and the logic has been modified to correctly enforce specific calculation rules as per project guidelines. This project is now considered complete, though additional functionality as well as better UX could be implemented.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Adam Koziorz
 // Odin-Calculator Project
-// JavaScript
+// JavaScript Component
 
 const numBtns = document.querySelectorAll(".num-btn");
 const opBtns = document.querySelectorAll(".op-btn");
@@ -14,6 +14,10 @@ const currentOp = document.querySelector("#current-operation");
 const states = ["LEFT", "RIGHT"];
 let state = states[0];
 
+// It would be better to create a class instead of global mutable
+// variables, but this works good enough for the purpose of
+// this project (this is just practice after all)
+let computed = false;
 let leftSide = "";
 let operator = "";
 let rightSide = "";
@@ -25,43 +29,45 @@ let result = 0;
 // Math functions
 // **************
 
-function add(leftSide, rightSide) {
-    return Number(leftSide) + Number(rightSide);
-}
-
-function subtract(leftSide, rightSide) {
-    return Number(leftSide) - Number(rightSide);
-}
-
-function multiply(leftSide, rightSide) {
-    return Number(leftSide) * Number(rightSide);
-}
-
-function divide(leftSide, rightSide) {
-    return Number(leftSide) / Number(rightSide);
-}
-
-// "1000000" rounds the number to 6 decimal places (there are 10 zeroes)
+// "1000000" rounds the number to 6 decimal places (six zeroes)
 function round(number) {
     return Math.round(number * 1000000) / 1000000
 }
 
+function add(leftSide, rightSide) {
+    return round(Number(leftSide) + Number(rightSide));
+}
+
+function subtract(leftSide, rightSide) {
+    return round(Number(leftSide) - Number(rightSide));
+}
+
+function multiply(leftSide, rightSide) {
+    return round(Number(leftSide) * Number(rightSide));
+}
+
+function divide(leftSide, rightSide) {
+    return round(Number(leftSide) / Number(rightSide));
+}
+
+
 // "Operate" is a wrapper function that will call the corresponding
 // calculation functions (+, -, *, /) depending on operator
+// Returns a number on success and null on failure
 function operate(leftSide, operator, rightSide) {
     switch(operator) {
         case "+":
-            return round(add(leftSide, rightSide));
+            return add(leftSide, rightSide);
         case "-":
-            return round(subtract(leftSide, rightSide));
+            return subtract(leftSide, rightSide);
         case "×":
-            return round(multiply(leftSide, rightSide));
+            return multiply(leftSide, rightSide);
         case "÷":
             if (rightSide == "0") {
                 alert("Error: Division by Zero");
                 return null;
             }
-            return round(divide(leftSide, rightSide));
+            return divide(leftSide, rightSide);
         default:
             alert("Error: Unknown Operation");
             return null;
@@ -70,19 +76,42 @@ function operate(leftSide, operator, rightSide) {
 
 
 
-// ***************
-// Event Listeners
-// ***************
+// ***************************
+// Event Listeners and Helpers
+// ***************************
+
+// Resets variables
+function clear() {
+    leftSide = "";
+    operator = "";
+    rightSide = "";
+    result = 0;
+    currentOp.textContent = '\u200B';
+    lastOp.textContent = '\u200B';
+    state = states[0];
+    computed = false;
+}
+
+function present() {
+    currentOp.textContent = result;
+    leftSide = result;
+    rightSide = "";
+    computed = true;
+}
+
 
 // Add Event Listeners for the Numbers
 numBtns.forEach((button) => {
     button.addEventListener('click', () => {
+        if (computed) return;
         if (state == states[0]) {
+            if (button.textContent === "0" && !leftSide) return;
             leftSide += button.textContent;
-            currentOp.textContent = leftSide;
-        } else if (state == "RIGHT") {
+            lastOp.textContent += button.textContent;
+        } else if (state == states[1]) {
+            if (button.textContent === "0" && !rightSide) return;
             rightSide += button.textContent;
-            currentOp.textContent = rightSide;
+            lastOp.textContent += button.textContent;
         }
     })
 })
@@ -90,38 +119,33 @@ numBtns.forEach((button) => {
 // Add Event Listeners for the Operations
 opBtns.forEach((button) => {
     button.addEventListener('click', () => {
-        if (state == "LEFT") {
+        if (!leftSide) return;
+        else if (state == states[0]) {
             operator = button.textContent;
             lastOp.textContent = leftSide + operator;
             state = states[1];
-        } else {
+        } else if (state == states[1] && rightSide) {
             result = operate(leftSide, operator, rightSide);
-            operator = button.textContent;
-            currentOp.textContent = result;
-            lastOp.textContent = result + operator;
-            leftSide = result;
-            rightSide = "";
+            if (result === null) {
+                clear();
+            } else {
+                operator = button.textContent;
+                lastOp.textContent = result + operator;
+                present();
+            }
         }
+        computed = false;
     })
 })
 
 // Add individual event listeners for func-btns
-clearBtn.addEventListener('click', () => {
-    leftSide = "";
-    operator = "";
-    rightSide = "";
-    currentOp.textContent = '\u200B';
-    lastOp.textContent = '\u200B';
-    state = states[0];
-})
+clearBtn.addEventListener('click', clear);
 
 calculateBtn.addEventListener('click', () => {
     if (!leftSide || !rightSide || !operator) return;
     // If we didn't return, we're in a valid state to calculate
     result = operate(leftSide, operator, rightSide);
-    currentOp.textContent = result;
-    lastOp.textContent = lastOp.textContent + rightSide + " =";
-    leftSide = result;
-    rightSide = "";
+    lastOp.textContent = lastOp.textContent + " =";
+    present();
     state = states[0];
 })
